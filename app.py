0b144d7bf1b9998bdcae983222d26122f315d951
mauricio-toapanta-fisagrp <mauricio.toapanta@fisagrp.com>
2025-10-24 18:22:43 -0500

    conexion a 10

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+# app_a.py
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hola Mundo', 200
+def call_app_b():
+    try:
+        # Llamamos a la app B, que está en localhost:3001
+        response = requests.get('http://mi-servicio-m10.mauricio-toapanta-dev.svc.cluster.local:4000/')
+        return f'Respuesta de app B: {response.text}', response.status_code
+    except requests.exceptions.ConnectionError:
+        return 'Error: no se pudo conectar con app B', 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)

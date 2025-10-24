a06f60c71641cd69ee57fcadf2e5f99ba31a0fd7
mauricio-toapanta-fisagrp <mauricio.toapanta@fisagrp.com>
2025-10-24 18:23:51 -0500

    conexion a 10

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def call_app_b():
     try:
         # Llamamos a la app B, que está en localhost:3001
         response = requests.get('http://mi-servicio-m10.mauricio-toapanta-dev.svc.cluster.local:4000/')
-        return f'Respuesta de app B: {response.text}', response.status_code
+        return f'Respuesta de app 10: {response.text}', response.status_code
     except requests.exceptions.ConnectionError:
-        return 'Error: no se pudo conectar con app B', 500
+        return 'Error: no se pudo conectar con app 10', 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
